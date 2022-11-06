85fa965c82215def9d7e547c09d2cb4797e6ddcc
Md Shadman Matin (Piyal) <78369801+YamatoDX@users.noreply.github.com>
2022-11-06 22:21:39 +0600

    getProducts done

--- pages/api/orders/index.js
+++ pages/api/orders/index.js
@@ -38,6 +38,38 @@ export default async function (req, res) {
     });
   }
 
+  if (req.method === "GET") {
+    const productList = [
+      {
+        name: "Water, 500 mL",
+        price: 300,
+        enabled: true,
+        imageUrl:
+          "https://i.picsum.photos/id/0/5000/3333.jpg?hmac=_j6ghY5fCfSD6tvtcV74zXivkJSPIfR9B8w34XeQmvU",
+      },
+      {
+        name: "Milk, 1000mL",
+        price: 250,
+        enabled: false,
+        imageUrl:
+          "https://i.picsum.photos/id/1/5000/3333.jpg?hmac=Asv2DU3rA_5D1xSe22xZK47WEAN0wjWeFOhzd13ujW4",
+      },
+      {
+        name: "Rice, 1000g",
+        price: 1000,
+        enabled: true,
+        imageUrl:
+          "https://i.picsum.photos/id/2/5000/3333.jpg?hmac=_KDkqQVttXw_nM-RyJfLImIbafFrqLsuGO5YuHqD-qQ",
+      },
+    ];
+
+    return res.status(200).json({
+      success: true,
+      errors: null,
+      data: productList,
+    });
+  }
+
   return res.status(200).json({
     message: "API Method Unknown",
   });
